80cfd2d59a1667aa9e288f6822bb20a9d8e1b940
songtomtom <tom.song@luxrobo.com>
2023-02-08 00:13:32 +0900

    매인 스크린

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -28,8 +28,52 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
-          // Flexible(flex: 2, child: child),
-          // Flexible(flex: 1, child: child)
+          Flexible(
+            flex: 3,
+            child: Center(
+              child: IconButton(
+                iconSize: 120,
+                color: Theme.of(context).cardColor,
+                onPressed: () {},
+                icon: const Icon(Icons.play_circle_outline),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodoro',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context).textTheme.headline1!.color,
+                          ),
+                        ),
+                        Text(
+                          '0',
+                          style: TextStyle(
+                            fontSize: 58,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context).textTheme.headline1!.color,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
